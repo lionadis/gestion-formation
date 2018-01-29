75e191d9712352f37f2c5f6182b697cb5b265085
Borchani Yessine <borchaniz96@gmail.com>
2018-01-29 14:06:29 +0100

    some bootsrap

--- src/main/java/com/insat/gestionformation/controllers/IndexController.java
+++ src/main/java/com/insat/gestionformation/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.insat.gestionformation.controllers;
 
+import com.insat.gestionformation.models.User;
 import com.insat.gestionformation.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,7 +13,7 @@ public class IndexController {
     @Autowired
     EventService eventService;
     @RequestMapping("/")
-    public String index(Model model){
+    public String index(Model model, User user){
         model.addAttribute("events", eventService.getAllEvents());
         return "index";
     }
